5f029871c04d09cd523fc28250dd84b9c86f09ff
Bruno Cukic <>
2025-08-28 18:23:07 +0200

    Configure S3 upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
