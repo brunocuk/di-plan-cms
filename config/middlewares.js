0e0d3425e6e3f7ac24e78df4b83d641ef3783bc9
Bruno Cukic <>
2025-08-30 11:57:38 +0200

    Changed settings for aws s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,37 +1,42 @@
-module.exports = [
-  "strapi::errors",
+// ~/strapi-aws-s3/backend/config/middlewares.ts
+
+export default [
+  'strapi::errors',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
             "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "di-plan-assets.s3.eu-north-1.amazonaws.com", // change here
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'diplan-strapi-aws-s3-images-bucket.s3.eu-north-1.amazonaws.com',
           ],
-          "media-src": [
+          'media-src': [
             "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "di-plan-assets.s3.eu-north-1.amazonaws.com", // change here
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'diplan-strapi-aws-s3-images-bucket.s3.eu-north-1.amazonaws.com',
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  /* End of snippet */
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,17 @@
+// ~/strapi-aws-s3/backend/config/plugins.js
+    
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: 'aws-s3',
       providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-          },
-          region: env("AWS_REGION"),
-          params: {
-            ACL: env("AWS_ACL", "public-read"),
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-            Bucket: env("AWS_BUCKET"),
-          },
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_BUCKET'),
         },
       },
       actionOptions: {
